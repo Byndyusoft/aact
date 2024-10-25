9ce78a53b8c4dc6d8c7b17ec100d5fe7fbe43423
Ruslan Safin <ruslan.safin@byndyusoft.com>
2024-10-25 21:48:11 +0300

    вынес тест на ACL в отдельный файл

---
+++ test/acl.test.ts
@@ -0,0 +1,26 @@
+import { Container } from "./entities";
+import {
+  loadPlantumlElements,
+  mapContainersFromPlantumlElements,
+} from "./plantuml";
+
+const SystemExternalType = "System_Ext";
+
+describe("Architecture", () => {
+  let containersFromPuml: Container[];
+
+  beforeAll(async () => {
+    const pumlElements = await loadPlantumlElements("C4L2.puml");
+    containersFromPuml = mapContainersFromPlantumlElements(pumlElements).allContainers;
+  });
+
+  it("only acl can depend on external systems", () => {
+    for (const container of containersFromPuml) {
+      const externalRelations = container.relations.filter(
+        (r) => r.to.type === SystemExternalType,
+      );
+      if (!container.tags?.includes("acl") && externalRelations.length > 0) 
+        fail();
+    }
+  });
+});
