dbe0b2032c6cd4c28a98a71c78fc23300d5f069f
Ruslan Safin <ruslan.safin@byndyusoft.com>
2024-10-25 22:42:22 +0300

    добавил тест на DB per service

---
+++ test/crud.test.ts
@@ -0,0 +1,26 @@
+import { Container } from "./entities";
+import {
+  loadPlantumlElements,
+  mapContainersFromPlantumlElements,
+} from "./plantuml";
+
+const ContainerDb = "ContainerDb";
+
+describe("Architecture", () => {
+  let containersFromPuml: Container[];
+
+  beforeAll(async () => {
+    const pumlElements = await loadPlantumlElements("C4L2.puml");
+    containersFromPuml = mapContainersFromPlantumlElements(pumlElements).allContainers;
+  });
+
+  it("only crud can relate to DB", () => {
+    for (const container of containersFromPuml) {
+      const dbRelation = container.relations.filter(
+        (r) => r.to.type === ContainerDb,
+      );
+      if (!container.tags?.includes("repo") && dbRelation.length > 0) 
+        fail();
+    }
+  });
+});
